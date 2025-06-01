a521111e44644b54f9c2e7b2c698ec283517388c
Jefferson Alves <jeffersoonsilva00@gmail.com>
2025-05-31 21:11:46 -0300

    Corrigindo erros do método post

--- src/main/java/ordem_de_servico/example/ordem_de_servico/dto/EquipamentoDto.java
+++
@@ -1,9 +0,0 @@
-package ordem_de_servico.example.ordem_de_servico.dto;
-
-import jakarta.validation.constraints.NotBlank;
-import ordem_de_servico.example.ordem_de_servico.model.OrdemServico;
-
-import java.util.List;
-
-public record EquipamentoDto( String tipo, String marca) {
-}
